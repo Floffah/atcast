e561d51fa3d6b36a3bfd3cfbf8001f5c1b3eb2cd
Floffah <floffah@floffah.dev>
2024-12-14 15:04:18 +0000

    chore: better logging in ci db migration script

--- packages/models/scripts/migrate.ts
+++ packages/models/scripts/migrate.ts
@@ -3,8 +3,11 @@ import { migrate } from "drizzle-orm/neon-http/migrator";
 
 import { db } from "@/client";
 
-if (process.env.NODE_ENV === "production" && process.env.DATABASE_URL) {
+if (process.env.CI == "1" && process.env.DATABASE_URL) {
+    console.log("Migrating database...");
     migrate(db, {
         migrationsFolder: "./drizzle",
     });
+} else {
+    console.log("Skipping database migration");
 }
