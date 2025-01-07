4dc3acfe8a406761e980dc3e0455eddb11d995c2
Floffah <floffah@floffah.dev>
2025-01-07 23:34:23 +0000

    chore: replace stylex configs with recommended

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,14 +1,33 @@
-import stylexConfig from "./stylex.config";
 import stylexPlugin from "@stylexswc/nextjs-plugin";
 import type { NextConfig } from "next";
+import path from "node:path";
 import Icons from "unplugin-icons/webpack";
 
-const withStylex = stylexPlugin(stylexConfig);
+const projectRoot = __dirname;
+// const monorepoRoot = path.resolve(projectRoot, "../../");
+
+const withStylex = stylexPlugin({
+    rsOptions: {
+        aliases: {
+            "@/*": [path.join(projectRoot, "src/*")],
+        },
+        unstable_moduleResolution: {
+            type: "commonJS",
+            // rootDir: monorepoRoot,
+        },
+    },
+    extractCSS: false,
+});
 
 const nextConfig = {
     reactStrictMode: true,
     serverExternalPackages: ["@node-rs/bcrypt"],
+    transpilePackages: ["@stylexjs/open-props"],
+    experimental: {
+        // ppr: true,
+    },
     typescript: {
+        // part of lint step, next ignores tsconfig references and breaks trpc
         ignoreBuildErrors: true,
     },
     eslint: {
@@ -25,9 +44,6 @@ const nextConfig = {
         );
 
         return config;
-    },
-    images: {
-        remotePatterns: [],
     },
 } satisfies NextConfig;
 
--- apps/web/postcss.config.js
+++ apps/web/postcss.config.js
@@ -1,10 +1,26 @@
+const path = require("path");
+
 const projectRoot = __dirname;
+const monorepoRoot = path.resolve(projectRoot, "../../");
 
 module.exports = {
     plugins: {
         "@stylexswc/postcss-plugin": {
-            include: [projectRoot + "/src/**/*.{js,jsx,ts,tsx}"],
-            ...require("./stylex.config"),
+            include: ["src/**/*.{js,jsx,ts,tsx}"],
+            rsOptions: {
+                isDev: process.env.NODE_ENV === "development",
+                genConditionalClasses: true,
+                treeshakeCompensation: true,
+                useRemForFontSize: true,
+                aliases: {
+                    "@/*": [path.join(projectRoot, "src/*")],
+                },
+                unstable_moduleResolution: {
+                    type: "commonJS",
+                    rootDir: projectRoot,
+                },
+            },
+            useCSSLayers: true,
         },
         "postcss-preset-env": {},
     },
