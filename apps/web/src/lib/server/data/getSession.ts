c669781b8ba9bd4547dd911fbf2c4c8187e35bb4
Floffah <floffah@floffah.dev>
2025-02-09 00:17:13 +0000

    chore: clean up dependencies

--- apps/web/src/lib/server/data/getSession.ts
+++ apps/web/src/lib/server/data/getSession.ts
@@ -1,5 +1,3 @@
-import { JoseKey } from "@atproto/jwk-jose";
-import { eq } from "drizzle-orm";
 import { RequestCookies } from "next/dist/compiled/@edge-runtime/cookies";
 import type { ReadonlyHeaders } from "next/dist/server/web/spec-extension/adapters/headers";
 import type { ReadonlyRequestCookies } from "next/dist/server/web/spec-extension/adapters/request-cookies";
@@ -7,9 +5,9 @@ import { cookies, headers } from "next/headers";
 import { NextRequest } from "next/server";
 import { cache } from "react";
 
-import { createDPoPFetch, getClientId } from "@atcast/atproto";
+import { ensureSessionValid } from "@atcast/atproto";
 import { SESSION_TOKEN } from "@atcast/lib";
-import { db, userSessions } from "@atcast/models";
+import { db } from "@atcast/models";
 
 import { getBskyAuthInfo } from "@/lib/oauth/bsky";
 import { AtprotoErrorResponse } from "@/lib/server/AtprotoErrorResponse";
@@ -109,63 +107,9 @@ export async function getSession(
         where: (users, { eq }) => eq(users.id, session!.userId),
     });
 
-    if (
-        session.accessTokenExpiresAt &&
-        session.accessTokenExpiresAt < new Date()
-    ) {
-        const key = await JoseKey.fromJWK(session.jwk as any);
-        const bskyOauthSpec = await getBskyAuthInfo();
-
-        const dpopFetch = createDPoPFetch({
-            key,
-            metadata: bskyOauthSpec,
-        });
-
-        const refreshResponse = await dpopFetch(bskyOauthSpec.token_endpoint, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                client_id: getClientId(),
-                grant_type: "refresh_token",
-                refresh_token: session.refreshToken,
-            }),
-        });
-        const refreshData = await refreshResponse.json();
-
-        if (refreshData.error) {
-            return {
-                errorResponse: new AtprotoErrorResponse(
-                    {
-                        error: "InvalidToken",
-                    },
-                    {
-                        status: 401,
-                    },
-                ),
-            };
-        }
-
-        const insertResult = await db
-            .update(userSessions)
-            .set({
-                accessToken: refreshData.access_token,
-                accessTokenExpiresAt: new Date(
-                    Date.now() + refreshData.expires_in * 1000,
-                ),
-                refreshToken: refreshData.refresh_token,
-                accessTokenType: refreshData.token_type,
-            })
-            .where(eq(userSessions.id, session.id))
-            .returning();
-
-        if (!insertResult) {
-            throw new Error("Failed to update session");
-        }
-
-        session = insertResult[0];
-    }
+    session = await ensureSessionValid(session, {
+        metadata: await getBskyAuthInfo(),
+    });
 
     return {
         session,
